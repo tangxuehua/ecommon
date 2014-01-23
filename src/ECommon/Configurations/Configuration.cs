98034dcef27a2595e9c12350054b696420625c04
xuehua <txh_723@163.com>
2014-01-23 22:02:58 +0800

    add extension files.

---
+++ src/ECommon/Configurations/Configuration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ECommon.IoC;
+
+namespace ECommon.Configurations
+{
+    public class Configuration
+    {
+        public static Configuration Instance { get; private set; }
+
+        private Configuration() { }
+
+        public static Configuration Create()
+        {
+            if (Instance != null)
+            {
+                throw new Exception("Could not create configuration instance twice.");
+            }
+            Instance = new Configuration();
+            return Instance;
+        }
+
+        public Configuration SetDefault<TService, TImplementer>(LifeStyle life = LifeStyle.Singleton)
+            where TService : class
+            where TImplementer : class, TService
+        {
+            ObjectContainer.Register<TService, TImplementer>(life);
+            return this;
+        }
+        public Configuration SetDefault<TService, TImplementer>(TImplementer instance)
+            where TService : class
+            where TImplementer : class, TService
+        {
+            ObjectContainer.RegisterInstance<TService, TImplementer>(instance);
+            return this;
+        }
+    }
+}
---
+++ src/Extensions/ECommon.JsonNet/ConfigurationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using ECommon.Configurations;
+using ECommon.Serializing;
+
+namespace ECommon.JsonNet
+{
+    /// <summary>ECommon configuration class JsonNet extensions.
+    /// </summary>
+    public static class ConfigurationExtensions
+    {
+        /// <summary>Use Json.Net as the json serializer for the equeue framework.
+        /// </summary>
+        /// <returns></returns>
+        public static Configuration UseJsonNet(this Configuration configuration)
+        {
+            configuration.SetDefault<IJsonSerializer, NewtonsoftJsonSerializer>(new NewtonsoftJsonSerializer());
+            return configuration;
+        }
+    }
+}
