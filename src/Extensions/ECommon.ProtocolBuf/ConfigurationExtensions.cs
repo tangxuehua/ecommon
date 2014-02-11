2bc273c6c8479d90e2896237479303feaaa4410b
xuehua <txh_723@163.com>
2014-02-11 21:49:33 +0800

    add a protobuf-net extension.

---
+++ src/Extensions/ECommon.ProtocolBuf/ConfigurationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using ECommon.Configurations;
+using ECommon.Serializing;
+
+namespace ECommon.ProtocolBuf
+{
+    /// <summary>ECommon configuration class ProtocolBuf extensions.
+    /// </summary>
+    public static class ConfigurationExtensions
+    {
+        /// <summary>Use ProtocolBufSerializer as the binary serializer.
+        /// </summary>
+        /// <returns></returns>
+        public static Configuration UseProtoBufSerializer(this Configuration configuration)
+        {
+            configuration.SetDefault<IBinarySerializer, ProtocolBufSerializer>(new ProtocolBufSerializer());
+            return configuration;
+        }
+    }
+}
---
+++ src/Extensions/ECommon.ProtocolBuf/ProtocolBufSerializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using ECommon.Serializing;
+using ProtoBuf;
+
+namespace ECommon.ProtocolBuf
+{
+    public class ProtocolBufSerializer : IBinarySerializer
+    {
+        public byte[] Serialize(object obj)
+        {
+            using (var stream = new MemoryStream())
+            {
+                Serializer.Serialize(stream, obj);
+                return stream.ToArray();
+            }
+        }
+        public T Deserialize<T>(byte[] data) where T : class
+        {
+            using (var stream = new MemoryStream(data))
+            {
+                return Serializer.Deserialize<T>(stream);
+            }
+        }
+        public object Deserialize(byte[] data, Type type)
+        {
+            using (var stream = new MemoryStream(data))
+            {
+                return Serializer.NonGeneric.Deserialize(type, stream);
+            }
+        }
+    }
+}
