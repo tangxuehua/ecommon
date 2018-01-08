bac7f4b9a316017fedfe8da835e13ff0a7b42243
tangxuehua <txh_723@163.com>
2018-01-09 00:17:41 +0800

    去掉dapper扩展代码中对sql server特定依赖的代码。

--- src/ECommon.Dapper/SqlMapperExtensions.cs
+++ src/ECommon.Dapper/SqlMapperExtensions.cs
@@ -30,7 +30,7 @@ namespace ECommon.Dapper
             var properties = GetProperties(obj);
             var columns = string.Join(",", properties);
             var values = string.Join(",", properties.Select(p => "@" + p));
-            var sql = string.Format("insert into {0} ({1}) values ({2}) select cast(scope_identity() as bigint)", table, columns, values);
+            var sql = string.Format("insert into {0} ({1}) values ({2})", table, columns, values);
 
             return connection.ExecuteScalar<long>(sql, obj, transaction, commandTimeout);
         }
@@ -48,7 +48,7 @@ namespace ECommon.Dapper
             var properties = GetProperties(obj);
             var columns = string.Join(",", properties);
             var values = string.Join(",", properties.Select(p => "@" + p));
-            var sql = string.Format("insert into {0} ({1}) values ({2}) select cast(scope_identity() as bigint)", table, columns, values);
+            var sql = string.Format("insert into {0} ({1}) values ({2})", table, columns, values);
 
             return connection.ExecuteScalarAsync<long>(sql, obj, transaction, commandTimeout);
         }
@@ -257,97 +257,6 @@ namespace ECommon.Dapper
         public static Task<IEnumerable<T>> QueryListAsync<T>(this IDbConnection connection, object condition, string table, string columns = "*", bool isOr = false, IDbTransaction transaction = null, int? commandTimeout = null)
         {
             return connection.QueryAsync<T>(BuildQuerySQL(condition, table, columns, isOr), condition, transaction, commandTimeout);
-        }
-
-        /// <summary>Query paged data from a single table.
-        /// </summary>
-        /// <param name="connection"></param>
-        /// <param name="condition"></param>
-        /// <param name="table"></param>
-        /// <param name="orderBy"></param>
-        /// <param name="pageIndex"></param>
-        /// <param name="pageSize"></param>
-        /// <param name="columns"></param>
-        /// <param name="isOr"></param>
-        /// <param name="transaction"></param>
-        /// <param name="commandTimeout"></param>
-        /// <returns></returns>
-        public static IEnumerable<dynamic> QueryPaged(this IDbConnection connection, dynamic condition, string table, string orderBy, int pageIndex, int pageSize, string columns = "*", bool isOr = false, IDbTransaction transaction = null, int? commandTimeout = null)
-        {
-            return QueryPaged<dynamic>(connection, condition, table, orderBy, pageIndex, pageSize, columns, isOr, transaction, commandTimeout);
-        }
-        /// <summary>Query paged data async from a single table.
-        /// </summary>
-        /// <param name="connection"></param>
-        /// <param name="condition"></param>
-        /// <param name="table"></param>
-        /// <param name="orderBy"></param>
-        /// <param name="pageIndex"></param>
-        /// <param name="pageSize"></param>
-        /// <param name="columns"></param>
-        /// <param name="isOr"></param>
-        /// <param name="transaction"></param>
-        /// <param name="commandTimeout"></param>
-        /// <returns></returns>
-        public static Task<IEnumerable<dynamic>> QueryPagedAsync(this IDbConnection connection, dynamic condition, string table, string orderBy, int pageIndex, int pageSize, string columns = "*", bool isOr = false, IDbTransaction transaction = null, int? commandTimeout = null)
-        {
-            return QueryPagedAsync<dynamic>(connection, condition, table, orderBy, pageIndex, pageSize, columns, isOr, transaction, commandTimeout);
-        }
-        /// <summary>Query paged data from a single table.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="connection"></param>
-        /// <param name="condition"></param>
-        /// <param name="table"></param>
-        /// <param name="orderBy"></param>
-        /// <param name="pageIndex"></param>
-        /// <param name="pageSize"></param>
-        /// <param name="columns"></param>
-        /// <param name="isOr"></param>
-        /// <param name="transaction"></param>
-        /// <param name="commandTimeout"></param>
-        /// <returns></returns>
-        public static IEnumerable<T> QueryPaged<T>(this IDbConnection connection, dynamic condition, string table, string orderBy, int pageIndex, int pageSize, string columns = "*", bool isOr = false, IDbTransaction transaction = null, int? commandTimeout = null)
-        {
-            var conditionObj = condition as object;
-            var whereFields = string.Empty;
-            var properties = GetProperties(conditionObj);
-            if (properties.Count > 0)
-            {
-                var separator = isOr ? " OR " : " AND ";
-                whereFields = " WHERE " + string.Join(separator, properties.Select(p => p + " = @" + p));
-            }
-            var sql = string.Format("SELECT {0} FROM (SELECT ROW_NUMBER() OVER (ORDER BY {1}) AS RowNumber, {0} FROM {2}{3}) AS Total WHERE RowNumber >= {4} AND RowNumber <= {5}", columns, orderBy, table, whereFields, (pageIndex - 1) * pageSize + 1, pageIndex * pageSize);
-
-            return connection.Query<T>(sql, conditionObj, transaction, true, commandTimeout);
-        }
-        /// <summary>Query paged data async from a single table.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="connection"></param>
-        /// <param name="condition"></param>
-        /// <param name="table"></param>
-        /// <param name="orderBy"></param>
-        /// <param name="pageIndex"></param>
-        /// <param name="pageSize"></param>
-        /// <param name="columns"></param>
-        /// <param name="isOr"></param>
-        /// <param name="transaction"></param>
-        /// <param name="commandTimeout"></param>
-        /// <returns></returns>
-        public static Task<IEnumerable<T>> QueryPagedAsync<T>(this IDbConnection connection, dynamic condition, string table, string orderBy, int pageIndex, int pageSize, string columns = "*", bool isOr = false, IDbTransaction transaction = null, int? commandTimeout = null)
-        {
-            var conditionObj = condition as object;
-            var whereFields = string.Empty;
-            var properties = GetProperties(conditionObj);
-            if (properties.Count > 0)
-            {
-                var separator = isOr ? " OR " : " AND ";
-                whereFields = " WHERE " + string.Join(separator, properties.Select(p => p + " = @" + p));
-            }
-            var sql = string.Format("SELECT {0} FROM (SELECT ROW_NUMBER() OVER (ORDER BY {1}) AS RowNumber, {0} FROM {2}{3}) AS Total WHERE RowNumber >= {4} AND RowNumber <= {5}", columns, orderBy, table, whereFields, (pageIndex - 1) * pageSize + 1, pageIndex * pageSize);
-
-            return connection.QueryAsync<T>(sql, conditionObj, transaction, commandTimeout);
         }
 
         private static string BuildQuerySQL(dynamic condition, string table, string selectPart = "*", bool isOr = false)
